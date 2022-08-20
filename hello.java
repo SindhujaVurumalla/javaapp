f74e96a82dde08f8d7c8205e0267a62c58da0705
Naveen Bhatta <er.naveenbatta@gmail.com>
2022-08-20 11:14:44 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
    public static void main(String[] args) {
-    System.out.println("Hello World - 18th August 2022 - Afternoon 02:26 PM - triggered by webhook");
+    System.out.println("Hello push from polling");
     }
 }
