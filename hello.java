7cb53a75725d551404e27c827070ebcae1723fef
Naveen Bhatta <er.naveenbatta@gmail.com>
2022-08-21 21:25:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
    public static void main(String[] args) {
-    System.out.println("Hello poll from polling");
+    System.out.println("Hello, Integration with Jenkins");
     }
 }
