e011114a3e93c6006d6bb662273ab48a420998e8
Naveen Bhatta <er.naveenbatta@gmail.com>
2022-08-21 22:19:01 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
    public static void main(String[] args) {
-    System.out.println("Hello, Integration with Jenkins with push events.");
+    System.out.println("Hello, Integration with Jenkins with push events. Webhook integration.");
     }
 }
