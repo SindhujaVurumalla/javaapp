a8c22ed8b6007d3a411564e3252b049fafaab8a0
SindhujaVurumalla <107925191+SindhujaVurumalla@users.noreply.github.com>
2022-09-11 13:29:56 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
    public static void main(String[] args) {
-    System.out.println("Hello, Integration with Jenkins with push events. Webhook integration.");
+    System.out.println("Hello, Integration with Jenkins with Jenkins.");
     }
 }
